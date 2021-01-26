aba86b5a27bcdfb787709bc12aad33400b881f92
John Ahn <69434435+john-yo-ahn@users.noreply.github.com>
2021-01-25 20:16:15 -0800

    feat: firebase authentication and splash screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import * as firebase from "firebase";
+import { loggingOut } from "../API/firebaseMethods";
+import WelcomeScreen from "../App"
 
 const TabIcon = (props) => (
   <Ionicons
@@ -10,19 +14,53 @@ const TabIcon = (props) => (
   />
 );
 
-export default class Home extends React.Component {
-  static navigationOptions = {
-      tabBarIcon: TabIcon
+const Home = () => {
+  let currentUserUID = firebase.auth().currentUser.uid;
+  const [firstName, setFirstName] = useState("");
+  const [loggedOut, setLoggedOut] = useState("false")
+
+  useEffect(() => {
+    async function getUserInfo() {
+      let doc = await firebase
+        .firestore()
+        .collection("users")
+        .doc(currentUserUID)
+        .get();
+
+      if (!doc.exists) {
+        Alert.alert("No user data found!");
+      } else {
+        let dataObj = doc.data();
+        setFirstName(dataObj.firstName);
+      }
+    }
+    getUserInfo();
+  });
+
+  const handlePress = () => {
+    loggingOut();
+    setLoggedOut(true)
   };
-  render() {
+  if (this.loggedOut) {
+    return (<WelcomeScreen />)
+  } else {
     return (
       <View style={styles.container}>
-        <Text>You Can Do it!!!</Text>
-        <Text>Home</Text>
+        {this.state}
+        <Text>Dashboard</Text>
+        <Text>Hi {firstName}</Text>
+        <TouchableOpacity onPress={handlePress}>
+          <Text>Log Out</Text>
+        </TouchableOpacity>
       </View>
     );
   }
-}
+};
+
+Home["navigationOptions"] = (screenProps) => ({
+  tabBarIcon: TabIcon,
+});
+
 
 const styles = StyleSheet.create({
   container: {
@@ -32,3 +70,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+export default Home
