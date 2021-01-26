31d565b647def511819e2e71b58e6e25a2218638
John Ahn <69434435+john-yo-ahn@users.noreply.github.com>
2021-01-25 20:17:41 -0800

    feat: added firebase authentication + splash screen

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+export default {
+  firebaseConfig: {
+    apiKey: "AIzaSyB1VQhBG3J_3O2jVovF2-lSVdWODguclfU",
+    authDomain: "werun-a7b97.firebaseapp.com",
+    databaseURL: "werun-a7b97",
+    projectId: "werun-a7b97.appspot.com",
+    storageBucket: "80793223082",
+    messagingSenderId: "1:80793223082:web:58ef70db4a5ceff9d975d3",
+    appId: "G-5VQ1RBZWKV",
+  },
+};
